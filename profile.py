d1bf31b4277df518fe28e663cb0293ea6323d50d
JaredBrose <128941355+JaredBrose@users.noreply.github.com>
2023-03-25 18:14:32 -0700

    Create profile.py

---
+++ profile.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+
+print(f"Name: {name}")
+print(f"Email: {email}")
+print(f"Password: {password}")
